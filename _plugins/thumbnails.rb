e733902c8ff6882134845a98530095dd2cf0ea3e
Shahan Khan <contact@shahan.me>
2014-07-10 16:43:15 -0700

    Thumbnail: Thumbnail generation not really working yet

--- _plugins/thumbnails.rb
+++ _plugins/thumbnails.rb
@@ -16,7 +16,7 @@ module Jekyll
 			end
 
 			@img = data
-			@imgThumbnail = @img;
+			@imgThumbnail = data.sub(File.extname(data), "-thumb#{File.extname(data)}");
 		end
 
 		def render(context)
@@ -24,9 +24,52 @@ module Jekyll
 		end
 	end
 
-	# class ThumbGenerator < Generator
-		
-	# end
+	class ThumbGenerator < Generator
+		def generate(site)
+			@from  = File.expand_path("images")
+			@to = File.expand_path(File.join(site.dest, "images"))
+
+			# need to loop each project
+			site.collections['projects'].docs.each do |project|
+				if project.data['home']
+					@gallery_dir = "#{@from}/#{project.data['project']}"
+					@gallery_dest = "#{@to}/#{project.data['project']}"
+					@static_dir = "images/#{project.data['project']}"
+					thumbify(files_to_resize(site))
+				end
+			end
+		end
+
+		def files_to_resize(site)
+			to_resize = []
+
+			Dir.glob(File.join(@gallery_dir, "**", "*.{png,jpg,jpeg,gif}")).each do |file|
+				puts file
+				if !File.basename(file).include? "-thumb"
+					name = File.basename(file).sub(File.extname(file), "-thumb#{File.extname(file)}")
+					thumbname = File.join(@gallery_dest, name)
+					# Keep the thumb files from being cleaned by Jekyll
+					# site.static_files << Jekyll::StaticFile.new(site, site.dest, @static_dir, name )
+					if !File.exists?(thumbname)
+						to_resize.push({ "file" => file, "thumbname" => thumbname })
+					end
+				end
+			end
+
+			return to_resize
+		end
+
+		def thumbify(items)
+			if items.count > 0
+				items.each do |item|
+					img = Magick::Image.read(item['file']).first
+					thumb = img.resize_to_fit!(360)
+					thumb.write(item['thumbname'])
+					thumb.destroy!
+				end
+			end
+		end
+	end
 end
 
 Liquid::Template.register_tag('thumbnail', Jekyll::ThumbnailTag)
